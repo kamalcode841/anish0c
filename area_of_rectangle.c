b9a0062b076ce9384b16c0ca11635e46b49d81f8
kamalcode841 <anishpanthi06@gmail.com>
2026-07-07 20:46:54 +0545

    Add files via upload
    this code is useful for findout the area of rectangle

---
+++ area_of_rectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int l,b,a;
+printf("enter the value of length");
+scanf("%d",&l);
+printf("enter the value of breadth");
+scanf("%d",&b);
+a=l*b;
+printf("the area is %d", a);
+getch();
+}
